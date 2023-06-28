47be38a39876dd9d6b2acd00f5b1069423254d33
+ <lucy.njoroge1@student.moringaschool.com>
2023-06-28 12:56:14 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+  // Remove the DOM node 'main#main'
+const mainNode = document.querySelector('main#main');
+if (mainNode) {
+  mainNode.remove();
+}
+
+// Create a new <h1> element with id 'victory' and text content
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion';
+
+// Append the new <h1> element to the document body
+document.body.appendChild(newHeader);
